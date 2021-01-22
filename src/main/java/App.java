b858c4f757bf229d7b4916bb1845c205985e1415
Ian Mukunya <imukunya@safaricom.co.ke>
2021-01-22 19:02:50 +0300

    add of ui template files plus database connection on app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,37 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.sql2o.*;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
 public class App {
+    public static Boolean connectionStatus = false;
     public static void main(String[] args) {
 
+
+        staticFileLocation("/public");
+
+        String connectionString = "jdbc:postgresql://localhost:5432/technology";      //connect to todolist, not todolist_test!
+        Sql2o sql2o = new Sql2o(connectionString, "admin", "admin");
+        testConnection(sql2o);
+        if(connectionStatus){
+            System.out.print("Connected");
+        }else{
+            System.out.print("not connected!");
+        }
+    }
+
+    public static Boolean testConnection(Sql2o sqlConn){
+
+        try(Connection conn =  sqlConn.open()){
+            System.out.print("Connected to the data base");
+            connectionStatus = true;
+        }catch (Sql2oException e){
+            System.out.print("error connecting to the data base");
+            connectionStatus = false;
+        }
+        return connectionStatus;
     }
 }
--- src/main/resources/dbscripts/db.sql
+++ src/main/resources/dbscripts/db.sql
@@ -1,12 +1,11 @@
-CREATE TABLE `departments` ( `id` INT NOT NULL , `name` INT );
+CREATE TABLE departments("id" INT, "name" TEXT );
 
-CREATE TABLE `staff` ( `staff_id`, `names` TEXT, `staff_number` TEXT);
+CREATE TABLE staff( "staff_id", "names" TEXT, "staff_number" TEXT);
 
-CREATE TABLE `roles` ( `role_id` INT , `role_name` );
+CREATE TABLE roles("role_id" INT , "role_name" );
 
-CREATE TABLE `staff_roles` ( `staff_id` INT, `role_id` INT)
+CREATE TABLE staff_roles( "staff_id" INT, "role_id" INT)
 
-CREATE TABLE `responsibilities` (`resp_id` int , `resp_name` text);
+CREATE TABLE responsibilities("resp_id" int , "resp_name" text);
 
-
-CREATE TABLE `staff_responsibilities` ( `resp_id` INT , `staff_id` INT )
+CREATE TABLE staff_responsibilities( "resp_id" INT , "staff_id" INT )
