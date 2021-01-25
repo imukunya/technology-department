3e4c7ff424a77de63ce98f960ed34826213bf7bb
Ian Mukunya <imukunya@safaricom.co.ke>
2021-01-25 08:21:52 +0300

    Heroku deploy

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -37,9 +37,20 @@ public class App {
     public static Boolean deleteStaffMsg = false;
 
     public static Boolean activeLink =false;
+
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+
     public static void main(String[] args) {
 
-
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
         String connectionString = "jdbc:postgresql://localhost:5432/technology";      //connect to todolist, not todolist_test!
--- src/main/resources/dbscripts/db.sql
+++ src/main/resources/dbscripts/db.sql
@@ -11,3 +11,25 @@ CREATE TABLE responsibilities("resp_id" int , "resp_name" text);
 CREATE TABLE staff_responsibilities( "resp_id" INT , "staff_id" INT )
 
 SELECT roles.name,roles.id,staff_roles.id,staff_roles.staff_id FROM roles INNER JOIN staff_roles ON roles.id = staff_roles.id WHERE staff_roles.staff_id ='';
+
+    ! This database is empty. If upgrading, you can transfer
+ ! data from another database with pg:copy
+Created postgresql-closed-07718 as DATABASE_URL
+
+Host
+ec2-52-206-44-27.compute-1.amazonaws.com
+
+Database
+d95n5un2ca5iaj
+
+User
+zwidsrmbldecpt
+
+Port
+5432
+
+Password
+b8849a518aa2c66093ee73eb2cd8c869b63c6a9f1929c5c32be2df6a4dbcf684
+
+URI
+postgres://zwidsrmbldecpt:b8849a518aa2c66093ee73eb2cd8c869b63c6a9f1929c5c32be2df6a4dbcf684@ec2-52-206-44-27.compute-1.amazonaws.com:5432/d95n5un2ca5iaj
